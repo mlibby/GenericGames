f74c8caa8e911c8d8be4b8fd671236726ad82059
Michael Libby <m@mlibby.com>
2024-01-20 17:08:47 -0600

    start work on providing link for users to join game server

---
+++ GenericGames/Helpers/ConnectionHelper.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace GenericGames.Helpers;
+
+public class ConnectionHelper
+{
+    public static string GetIpAddress()
+    {
+        string returnAddress = String.Empty;
+
+        // Get a list of all network interfaces (usually one per network card, dialup, and VPN connection)
+        NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+        foreach (NetworkInterface network in networkInterfaces)
+        {
+            // Read the IP configuration for each network
+            IPInterfaceProperties properties = network.GetIPProperties();
+
+            if (network.OperationalStatus == OperationalStatus.Up &&
+                   !network.Description.ToLower().Contains("virtual") &&
+                   !network.Description.ToLower().Contains("pseudo"))
+            {
+                // Each network interface may have multiple IP addresses
+                foreach (IPAddressInformation address in properties.UnicastAddresses)
+                {
+                    // We're only interested in IPv4 addresses for now
+                    if (address.Address.AddressFamily != AddressFamily.InterNetwork)
+                    {
+                        continue;
+                    }
+
+                    // Ignore loopback addresses (e.g., 127.0.0.1)
+                    if (IPAddress.IsLoopback(address.Address))
+                    {
+                        continue;
+                    }
+
+                    returnAddress = address.Address.ToString();
+                }
+            }
+        }
+
+        return returnAddress;
+    }
+}
---
+++ GenericGames/Pages/Index.razor.cs
@@ -0,0 +1,16 @@
+﻿using GenericGames.Helpers;
+
+namespace GenericGames.Pages;
+
+public partial class Index
+{
+    public string ServerUrl = "localhost";
+
+    protected override async Task OnInitializedAsync()
+    {
+        var baseUri = new Uri(this.NavigationManager.BaseUri);
+        var target = $"{baseUri.Scheme}://{ConnectionHelper.GetIpAddress()}:{baseUri.Port}/join";
+        var targetUri = new Uri(target);
+        ServerUrl = targetUri.ToString();
+    }
+}
