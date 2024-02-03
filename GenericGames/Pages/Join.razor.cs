5fb5bbe21b3f45f2f9e737a9336565bd6062bb76
Michael Libby <m@mlibby.com>
2024-02-03 17:08:57 -0600

    Stub /join page

---
+++ GenericGames/Pages/Join.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace GenericGames.Pages;
+
+public partial class Join
+{
+    private FormModel? model;
+    private EditContext? editContext;
+
+    protected override async Task OnInitializedAsync()
+    {
+        model = new FormModel();
+        //model.FormChanged += HandleFormChanged;
+        editContext = new EditContext(model);
+    }
+
+    public class FormModel
+    {
+        private string playerName = "";
+        public string PlayerName
+        {
+            get => playerName;
+            set
+            {
+                playerName = value;
+                OnFormChanged();
+            }
+        }
+
+        public event EventHandler? FormChanged = default!;
+        private void OnFormChanged() => FormChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
